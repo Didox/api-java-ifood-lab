ad5aeac949daa19ee59775d86a7361c104757691
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2022-04-05 05:40:19 -0300

    colocando um novo item na minha API

--- src/main/java/br/com/kintsugi/apirest/apirest/model_views/Home.java
+++ src/main/java/br/com/kintsugi/apirest/apirest/model_views/Home.java
@@ -4,4 +4,8 @@ public class Home {
   public String getMensagem(){
     return "Bem vindo a minha primeira API";
   }
+
+  public String getDocumentacao(){
+    return "TODO: Logo irei colocar o swagger nesta API";
+  }
 }
